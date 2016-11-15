ad5289f1b749448f0daf063cb3e9820dd0b030b6
benardop <benard.oc.p@gmail.com>
2016-11-15 17:03:32 -0200

    Add  custom ConnectionPool class

---
+++ src/farmfresh/data/ConnectionPool.java
@@ -0,0 +1,48 @@
+package farmfresh.data;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+/**
+ * Created by benard on 11/15/2016.
+ */
+public class ConnectionPool {
+    private static ConnectionPool pool = null;
+    private static DataSource dataSource = null;
+
+    public synchronized static ConnectionPool getInstance() {
+        if (pool == null) {
+            pool = new ConnectionPool();
+        }
+        return pool;
+    }
+
+    private ConnectionPool() {
+        try {
+            InitialContext ic = new InitialContext();
+            dataSource = (DataSource) ic.lookup("java:/comp/env/jdbc/farmFreshDB");
+        } catch (NamingException e) {
+            System.err.println(e);
+        }
+    }
+
+    public Connection getConnection() {
+        try {
+            return dataSource.getConnection();
+        } catch (SQLException sqle) {
+            System.err.println(sqle);
+            return null;
+        }
+    }
+
+    public void freeConnection(Connection c) {
+        try {
+            c.close();
+        } catch (SQLException sqle) {
+            System.err.println(sqle);
+        }
+    }
+}
